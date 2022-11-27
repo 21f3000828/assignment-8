f3b74913e62b995459d2c5d05602fdf41c8f196a
21f3000828 <21f3000828@student.onlinedegree.iitm.ac.in>
2022-11-27 18:46:35 +0530

    First commit

---
+++ app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+
+st.title('Subtraction of 2 given numbers')
+
+x = st.number_input('x')
+y = st.number_input('y')
+st.write('Subtraction is', x-y)
+
